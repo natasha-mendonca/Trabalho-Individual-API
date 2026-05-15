0697244e95e14a1646e12c311a7fdad2ff862001
natasha-mendonca <adm.natashamendonca@gmail.com>
2026-05-15 17:26:35 -0300

    inclusão de entidades

---
+++ trabalhoIndividual/src/main/java/org/serratec/trabalhoIndividual/entity/Cliente.java
@@ -0,0 +1,37 @@
+package org.serratec.trabalhoIndividual.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@NotNull
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Size(max = 60)
+    @Column
+    private String nome;
+
+    @Column(unique = true, length = 11)
+    private String cpf;
+
+    @Size(max=50)
+    @Column(unique = true, length = 50)
+    private String email;
+
+    @Column(length = 13)
+    private String telefone;
+
+}
---
+++ trabalhoIndividual/src/main/java/org/serratec/trabalhoIndividual/entity/Veiculo.java
@@ -0,0 +1,63 @@
+package org.serratec.trabalhoIndividual.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Veiculo {
+
+    @NotNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @NotNull
+    @Size(max = 60)
+    @Column
+    private String cliente;
+
+    @NotNull
+    @Column
+    private String marca;
+
+    @NotNull
+    @Column
+    private String modelo;
+
+    @NotNull
+    @Column(length = 4)
+    private int ano;
+
+    @NotNull
+    @Min(0)
+    @Column
+    private double valor;
+
+    @NotNull
+    @Column(unique = true)
+    private String placa;
+
+    @NotNull
+    @Column
+    private String maximoDesconto;
+
+    @NotNull
+    @Column
+    private boolean vendido;
+
+    @Min(0)
+    @Column
+    private double valorVenda;
+
+}
